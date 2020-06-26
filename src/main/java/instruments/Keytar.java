c5b1efe53094ce1c04cec803e7811b65e0ab4f86
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-26 14:50:30 +0100

    set up and test keytar class. all tests pass

--- src/main/java/instruments/Keytar.java
+++ src/main/java/instruments/Keytar.java
@@ -1,4 +1,11 @@
 package instruments;
 
-public class Keytar {
+public class Keytar extends Instrument{
+    public Keytar(String manufacturer, String material, int buyPrice, int sellPrice){
+        super(manufacturer, material, buyPrice, sellPrice);
+    }
+
+    public String play(){
+        return "wah wah wummm";
+    }
 }
--- src/test/java/instrumentsTest/KeytarTest.java
+++ src/test/java/instrumentsTest/KeytarTest.java
@@ -1,4 +1,48 @@
 package instrumentsTest;
 
+import instruments.Keytar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class KeytarTest {
+    private Keytar keytar;
+
+    @Before
+    public void setUp(){
+        keytar = new Keytar("Roland", "Plastic", 200, 300);
+    }
+
+    @Test
+    public void canBePlayed(){
+        assertEquals("wah wah wummm", keytar.play());
+    }
+
+    @Test
+    public void hasManufacturer(){
+        assertEquals("Roland", keytar.getManufacturer());
+    }
+
+    @Test
+    public void hasMaterialItIsMadeFrom(){
+        assertEquals("Plastic", keytar.getMaterial());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(200, keytar.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(300, keytar.getSellPrice());
+    }
+
+    @Test
+    public void canCalculateProfit(){
+        assertEquals(100, keytar.profitOnItem());
+    }
+
+
 }
