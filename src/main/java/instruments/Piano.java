184a486030b9d61c4f8860f9e8afeef6fd3dfcd8
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-26 15:15:47 +0100

    set up and test piano class. all tests pass

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,11 @@
 package instruments;
 
-public class Piano {
+public class Piano extends Instrument{
+    public Piano(String manufacturer, String material, int buyPrice, int sellPrice){
+        super(manufacturer, material, buyPrice, sellPrice);
+    }
+
+    public String play(){
+        return "plink plonk plink";
+    }
 }
--- src/test/java/instrumentsTest/PianoTest.java
+++ src/test/java/instrumentsTest/PianoTest.java
@@ -1,4 +1,47 @@
 package instrumentsTest;
 
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+    private Piano piano;
+
+    @Before
+    public void setUp(){
+        piano = new Piano("Steinway and Sons", "Maple", 500, 1100);
+    }
+
+    @Test
+    public void canBePlayed(){
+        assertEquals("plink plonk plink", piano.play());
+    }
+
+    @Test
+    public void canGetManufacturer(){
+        assertEquals("Steinway and Sons", piano.getManufacturer());
+    }
+
+    @Test
+    public void canGetMaterialItIsMadeFrom(){
+        assertEquals("Maple", piano.getMaterial());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(500, piano.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(1100, piano.getSellPrice());
+    }
+
+    @Test
+    public void canCalculateProfit(){
+        assertEquals(600, piano.profitOnItem());
+    }
+
 }
