3a8a4e0251374d2d5cbf8b9f632e78568a6f4529
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-26 14:14:20 +0100

    set up and test guitar class. all tests pass

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,11 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
+    public Guitar(String manufacturer, String material, int buyPrice, int sellPrice){
+        super(manufacturer, material, buyPrice, sellPrice);
+    }
+
+    public String play(){
+        return "strum strum";
+    }
 }
--- src/test/java/instrumentsTest/GuitarTest.java
+++ src/test/java/instrumentsTest/GuitarTest.java
@@ -1,4 +1,42 @@
 package instrumentsTest;
 
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+    private Guitar guitar;
+
+    @Before
+    public void setUp(){
+        guitar = new Guitar("Fender", "Rosewood", 300, 450);
+    }
+
+    @Test
+    public void hasManufacturer(){
+        assertEquals("Fender", guitar.getManufacturer());
+    }
+
+    @Test
+    public void canBePlayed(){
+        assertEquals("strum strum", guitar.play());
+    }
+
+    @Test
+    public void hasMaterialItIsMadeFrom(){
+        assertEquals("Rosewood", guitar.getMaterial());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(300, guitar.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(450, guitar.getSellPrice());
+    }
+
 }
